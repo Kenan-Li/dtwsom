9c7ab1514420b301291d441e0cc277adc9c5d712
Kenan Li <kenanlsu@gmail.com>
2019-09-27 14:15:14 -0700

    structure

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
+with open('Readme.md') as f:
+      long_description = f.read()
+
 setup(name='DtwSom',
-  version= '1.0',
-  description='Implementation of the Dynamic Time Warping Self Organizing Maps (DTW-SOM)',
-  author='Kenan Li',
-  package_data={'': ['Readme.md']},
-  include_package_data=True,
-  license="CC BY 3.0",
-  py_modules=['dtwsom'],
-  requires = ['numpy', 'fastdtw'],
-  url = 'https://github.com/Kenan-Li/dtwsom',
-  download_url = 'https://github.com/Kenan-Li/dtwsom/archive/master.zip',
-  keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
+      packages=['DtwSom'],
+      version='0.2.1',
+      description='Implementation of the Dynamic Time Warping Self Organizing Maps (DTW-SOM)',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Kenan Li',
+      author_email="kenanl@usc.edu",
+      license="CC BY 3.0",
+      install_requires=['numpy', 'fastdtw'],
+      url='https://github.com/Kenan-Li/dtwsom',
+      download_url='https://github.com/Kenan-Li/dtwsom/archive/master.zip',
+      keywords=['self-organizing map', 'time series analysis', 'clustering', 'dimentionality reduction', 'dynamic time warping']
  )
