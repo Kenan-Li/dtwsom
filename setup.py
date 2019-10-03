42c8caae485085050a212c367689c7a8ed0de93d
Kenan Li <kenanlsu@gmail.com>
2019-10-03 15:27:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import setuptools
 
-with open('Readme.md') as f:
-      long_description = f.read()
-
-setup(name='DtwSom',
-      packages=['DtwSom'],
-      version='0.2.1',
+setuptools.setup(name='DtwSom',
+      packages=setuptools.find_packages(),
+      version='0.2.3',
       description='Implementation of the Dynamic Time Warping Self Organizing Maps (DTW-SOM)',
-      long_description=long_description,
       long_description_content_type='text/markdown',
       author='Kenan Li',
       author_email="kenanl@usc.edu",
       license="CC BY 3.0",
-      install_requires=['numpy', 'fastdtw'],
+      install_requires=['numpy', 'fastdtw', 'scipy'],
       url='https://github.com/Kenan-Li/dtwsom',
       download_url='https://github.com/Kenan-Li/dtwsom/archive/master.zip',
       keywords=['self-organizing map', 'time series analysis', 'clustering', 'dimentionality reduction', 'dynamic time warping']
